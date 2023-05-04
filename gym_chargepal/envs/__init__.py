24e8bc22eb423a80176750505622b5863643042e
Georg John <georg.john@dfki.de>
2023-05-04 10:39:44 +0200

    Clean up

--- gym_chargepal/envs/__init__.py
+++ gym_chargepal/envs/__init__.py
@@ -4,17 +4,4 @@ import gym_chargepal.envs.env_config as config
 environment_register = {
     'ChargePal-Testbed-Reacher-3D-PositionControl-v1': config.testbed_reacher_3dof_position_ctrl_v1,
     'ChargePal-Testbed-Reacher-6D-PositionControl-v1': config.testbed_reacher_6dof_position_ctrl_v1,
-    # 'ChargePal-Testbed-Reacher-6D-PositionControl-v0': config.testbed_reacher_6dof_position_ctrl_v0,
-    # 'ChargePal-Testbed-Plugger-PositionControl-v0': config.testbed_plugger_position_ctrl_v0,
-
-    # 'ChargePal-Plugger-PositionControl-v0': config.plugger_position_ctrl_v0,
-    # 'ChargePal-Plugger-PositionControl-v1': config.plugger_position_ctrl_v1,
-    # 'ChargePal-Plugger-PositionControl-v2': config.plugger_position_ctrl_v2,
-
-    # 'ChargePal-Reacher-1D-PositionControl-v0': config.reacher_1dof_position_ctrl_v0,
-    # 'ChargePal-Reacher-3D-PositionControl-v0': config.reacher_3dof_position_ctrl_v0,
-    # 'ChargePal-Reacher-6D-PositionControl-v0': config.reacher_6dof_position_ctrl_v0,
-
-    # 'ChargePal-Reacher-3D-VelocityControl-v0': config.reacher_3dof_velocity_ctrl_v0,
-    # 'ChargePal-Reacher-6D-VelocityControl-v0': config.reacher_6dof_velocity_ctrl_v0,
 }
